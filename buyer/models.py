cad9fac00042240c7bb60113fbbefeabe56389e6
devvlopar <devangsingh34@gmail.com>
2023-01-17 19:38:58 +0530

    buyer table created

--- buyer/models.py
+++ buyer/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Buyer(models.Model):
+    first_name = models.CharField(max_length= 50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+
+
+
+
+
+
+
+# models = database ke andar Tables = models.py ke andar class
+# SQL : structured query langauge
+# ORM : Object Relational Mapping
